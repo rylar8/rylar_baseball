c40ba9fbbd6cb443fc50eb5e6d6199a5922a9bbf
Ryley Larson <rlarson.ump@gmail.com>
2023-02-13 21:38:41 -0500

    trying stuff

--- functions.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-
-def createGameObject(csv):
-    df = pd.read_csv(csv)
-    return df
---
+++ game.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+class Game:
+    def __init__(self, csv):
+        self.frame = pd.read_csv(csv)
+
+    def df(self):
+        return self.df
+
+def myFunc():
+    print(1)
+

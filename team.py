4570f09733216b2e60c45456e41165921e9fd288
Ryley Larson <rlarson.ump@gmail.com>
2023-02-16 17:28:20 -0500

    starting Team class

---
+++ team.py
@@ -0,0 +1,23 @@
+
+
+class Team():
+    def __init__(self):
+        self.teamID = 1
+
+    def players():
+        pass
+
+    def pitchers():
+        pass
+
+    def hitters():
+        pass
+
+    def catchers():
+        pass
+
+    def infielders():
+        pass
+
+    def outfielders():
+        pass

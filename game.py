b59a25271f04093f49b58e3948ab5395366f950e
Ryley Larson <rlarson.ump@gmail.com>
2023-03-10 11:32:22 -0500

    Finished toDatabase() method

--- game.py
+++ game.py
@@ -110,7 +110,7 @@ class Game:
             VALUES (?,?,?,?,?,?,?,?)''', (self.trackman_id, self.date, self.time, stadium_id, league_id, division_id, home_id, away_id))
             con.commit()
         except:
-            print(f'Game already in games table: {self.away.trackman_id} at {self.home.trackman_id} on {self.date} (gameID - {self.trackman_id})')
+            print(f'Game already in games table: {self.away.trackman_id} at {self.home.trackman_id} on {self.date} (gameID = {self.trackman_id})')
         #Add batters
         for batter_id in set(self.data['BatterId'].dropna()):
             batter_name = self.data[self.data['BatterId'] == batter_id].iloc[0]['Batter']
@@ -139,7 +139,7 @@ class Game:
             cur.execute('SELECT team_id FROM teams WHERE trackman_name = ? AND year = ?',
                         (pitcher_team, self.year))
             team_id = cur.fetchone()[0]
-            #Get batter side id
+            #Get pitcher side id
             pitcher_side = self.data[self.data['PitcherId'] == pitcher_id].iloc[0]['PitcherThrows']
             cur.execute('SELECT side_id FROM sides WHERE side = ?',
                         (pitcher_side,))
@@ -152,6 +152,127 @@ class Game:
             except:
                 pass
 
+        #Add catchers
+        for catcher_id in set(self.data['CatcherId'].dropna()):
+            catcher_name = self.data[self.data['CatcherId'] == catcher_id].iloc[0]['Catcher']
+            #Get team id
+            catcher_team = self.data[self.data['CatcherId'] == catcher_id].iloc[0]['PitcherTeam']
+            cur.execute('SELECT team_id FROM teams WHERE trackman_name = ? AND year = ?',
+                        (catcher_team, self.year))
+            team_id = cur.fetchone()[0]
+            #Get catcher side id
+            catcher_side = self.data[self.data['CatcherId'] == catcher_id].iloc[0]['CatcherThrows']
+            cur.execute('SELECT side_id FROM sides WHERE side = ?',
+                        (catcher_side,))
+            catcher_side_id = cur.fetchone()[0]
+            catcher_id = int(catcher_id)
+            try:
+                cur.execute('''INSERT INTO catchers (catcher_name, team_id, trackman_id, catcher_side_id)
+                VALUES (?, ?, ?, ?)''', (catcher_name, team_id, catcher_id, catcher_side_id))
+                con.commit()
+            except:
+                pass
+        #Write Trackman data
+        #Get game id
+        cur.execute('SELECT game_id FROM games WHERE trackman_id = ?', (self.trackman_id,))
+        game_id = cur.fetchone()[0]
+
+        pitches = []
+        for pitch in self.data.itertuples():
+            pitch_num = pitch.PitchNo
+            inning = pitch.Inning
+            #Get top bottom id
+            cur.execute('SELECT split_id FROM inning_split WHERE split = ?', (pitch._16,))
+            top_bottom_id = cur.fetchone()[0]
+            pa_of_inning = pitch.PAofInning
+            pitch_of_pa = pitch.PitchofPA
+            #Get pitcher id unless id does not exist, replace with example player
+            try:
+                cur.execute('SELECT pitcher_id FROM pitchers WHERE trackman_id = ?', (int(pitch.PitcherId),))
+                pitcher_id = cur.fetchone()[0]
+            except ValueError:
+                pitcher_id = 0
+            #Get batter id unless id does not exist, replace with example player
+            try:
+                cur.execute('SELECT batter_id FROM batters WHERE trackman_id = ?', (int(pitch.BatterId),))
+                batter_id = cur.fetchone()[0]
+            except ValueError:
+                batter_id = 0
+            #Get catcher id unless id does not exist, replace with example player
+            try:
+                cur.execute('SELECT catcher_id FROM catchers WHERE trackman_id = ?', (int(pitch.CatcherId),))
+                catcher_id = cur.fetchone()[0]
+            except ValueError:
+                catcher_id = 0
+            #Get league id
+            cur.execute('SELECT league_id FROM leagues WHERE trackman_name = ? AND year = ?', (self.league, self.year))
+            league_id = cur.fetchone()[0]
+            #Get division id
+            cur.execute('SELECT division_id FROM divisions WHERE trackman_name = ? AND year = ?', (self.division, self.year))
+            division_id = cur.fetchone()[0]
+            #Get home id
+            cur.execute('SELECT team_id FROM teams WHERE trackman_name = ? AND year = ?', (self.home.trackman_id, self.year))
+            home_id = cur.fetchone()[0]
+            #Get away id
+            cur.execute('SELECT team_id FROM teams WHERE trackman_name = ? AND year = ?', (self.away.trackman_id, self.year))
+            away_id = cur.fetchone()[0]
+            outs = pitch.Outs
+            balls = pitch.Balls
+            strikes = pitch.Strikes
+            velocity = pitch.RelSpeed
+            vertical = pitch.VertBreak
+            induced = pitch.InducedVertBreak
+            horizontal = pitch.HorzBreak
+            spin = pitch.SpinRate
+            axis = pitch.SpinAxis
+            tilt = pitch.Tilt
+            release_height = pitch.RelHeight
+            release_side = pitch.RelSide
+            release_extension = pitch.Extension
+            #Get auto type id
+            cur.execute('SELECT type_id FROM pitches WHERE auto_pitch = ?', (pitch.AutoPitchType,))
+            auto_type_id = cur.fetchone()[0]
+            #Get tagged type id
+            cur.execute('SELECT type_id FROM pitches WHERE tagged_pitch = ?', (pitch.TaggedPitchType,))
+            tagged_type_id = cur.fetchone()[0]
+            #Get call id
+            cur.execute('SELECT type_id FROM calls WHERE call = ?', (pitch.PitchCall,))
+            call_id = cur.fetchone()[0]
+            location_height = pitch.PlateLocHeight
+            location_side = pitch.PlateLocSide
+            exit_velocity = pitch.ExitSpeed
+            launch_angle = pitch.Angle
+            hit_direction = pitch.Direction
+            hit_spin = pitch.HitSpinRate
+            #Get hit type id
+            cur.execute('SELECT type_id FROM hits WHERE hit = ?', (pitch.TaggedHitType,))
+            hit_type_id = cur.fetchone()[0]
+            distance = pitch.Distance
+            hang_time = pitch.HangTime
+            hit_bearing = pitch.Bearing
+            #Get result id
+            cur.execute('SELECT type_id FROM results WHERE result = ?', (pitch.PlayResult,))
+            result_id = cur.fetchone()[0]
+            outs_made = pitch.OutsOnPlay
+            runs_scored = pitch.RunsScored
+            catcher_velocity = pitch.ThrowSpeed
+            catcher_pop = pitch.PopTime
+
+            pitches.append((game_id, pitch_num, inning, top_bottom_id, pa_of_inning, pitch_of_pa,
+            pitcher_id, batter_id, catcher_id, league_id, division_id, home_id, away_id, outs, balls, strikes, velocity, 
+            vertical, induced, horizontal, spin, axis, tilt, release_height, release_side, release_extension, auto_type_id,
+            tagged_type_id, call_id, location_height, location_side, exit_velocity, launch_angle, hit_direction, hit_spin, 
+            hit_type_id, distance, hang_time, hit_bearing, result_id, outs_made, runs_scored, catcher_velocity, catcher_pop))
+
+
+        cur.executemany('''INSERT INTO trackman (game_id, pitch_num, inning, top_bottom_id, pa_of_inning, pitch_of_pa,
+        pitcher_id, batter_id, catcher_id, league_id, division_id, home_id, away_id, outs, balls, strikes, velocity, 
+        vertical, induced, horizontal, spin, axis, tilt, release_height, release_side, release_extension, auto_type_id,
+        tagged_type_id, call_id, location_height, location_side, exit_velocity, launch_angle, hit_direction, hit_spin, 
+        hit_type_id, distance, hang_time, hit_bearing, result_id, outs_made, runs_scored, catcher_velocity, catcher_pop)
+        VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, 
+        ?, ?, ?, ?, ?, ?, ?, ?, ?)''', pitches)
+        con.commit()
 
     def writeHitterReports():
         pass
